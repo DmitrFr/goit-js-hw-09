86021fb1a58824b2ac2ddb2cd0bf658f0ecef782
DmitrFr <122161314+DmitrFr@users.noreply.github.com>
2023-10-05 09:34:36 +0200

    task2

---
+++ src/js/02-timer.js
@@ -0,0 +1,65 @@
+// Описан в документации
+import flatpickr from 'flatpickr';
+// Дополнительный импорт стилей
+import 'flatpickr/dist/flatpickr.min.css';
+
+let date;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < Date.now()) {
+      alert('Please choose a date in the future');
+    } else {
+      date = selectedDates[0];
+      startButton.disabled = false;
+    }
+  },
+};
+const inputEl = document.querySelector('#datetime-picker');
+const startButton = document.querySelector('[data-start]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+interval = null;
+
+startButton.disabled = true;
+
+startButton.addEventListener('click', onStartButtonClick);
+
+function onStartButtonClick() {
+  interval = setInterval(() => {
+    const deltaTime = convertMs(date - Date.now());
+    daysEl.textContent = addLeadingZero(deltaTime.days);
+    hoursEl.textContent = addLeadingZero(deltaTime.hours);
+    minutesEl.textContent = addLeadingZero(deltaTime.minutes);
+    secondsEl.textContent = addLeadingZero(deltaTime.seconds);
+  }, 1000);
+}
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+flatpickr(inputEl, options);
